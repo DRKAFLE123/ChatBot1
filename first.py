249350b0d4d6d5157c68c8ce7f2deea01568bc42
DRKAFLE <damodarms0804@gmail.com>
2022-12-28 13:32:03 +0545

    this is first commit

---
+++ first.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print(3)
+# for i in range(10):
+#    if(i%2==0):
+#     print("I love u",end=' ')
+x=str(input("\n\nDo you Love me: "))
+for i in range(100):
+   if x=="yes":
+      print ("I love u too",end=" ")
+   else:
+      print("I hate you ",end=" ")
